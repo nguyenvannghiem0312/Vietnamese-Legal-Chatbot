80eda5524e269961c6b3a0f9a146fbe3e14f7cc7
NghiemAbe <nguyenvannghiem0312@gmail.com>
2024-05-22 17:39:40 +0700

    Add pretty

--- utils/log.py
+++ utils/log.py
@@ -15,3 +15,9 @@ def get_logger(name: str):
         handler.setFormatter(formatter)
         logger.addHandler(handler)
     return logger
+
+def prettify_source(source):
+    document = os.path.basename(source.get("document"))
+    score = source.get("score")
+    content_preview = source.get("content_preview")
+    return f"• **{document}** with score ({round(score,2)}) \n\n **Preview:** \n {content_preview} \n"
