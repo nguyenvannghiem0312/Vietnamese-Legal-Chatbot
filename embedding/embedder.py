f2f29f395acb9313eb3a83fa140660122236a9be
NghiemAbe <nguyenvannghiem0312@gmail.com>
2024-05-23 16:21:42 +0800

    Update new model for embediing

--- embedding/embedder.py
+++ embedding/embedder.py
@@ -13,6 +13,6 @@ class Embedder(ABC):
 
 class EmbedderHuggingFace(Embedder):
     def __init__(self,
-                model_name: str = "bkai-foundation-models/vietnamese-bi-encoder",
+                model_name: str = "NghiemAbe/Vi-Legal-Roberta-grad-cache-256-gist",
                 ):
         self.embedder = HuggingFaceEmbeddings(model_name=model_name)
